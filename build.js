464a882a100763bb914d406da7079686b5c3a90b
Erik Sälgström Peterson <erik@swedenunlimited.com>
2015-04-18 01:56:54 -0400

    Add brfs transform to build

--- build.js
+++ build.js
@@ -1,13 +1,12 @@
 var open = require('fs').open;
 var browserify = require('browserify');
 
-
 var build = function(){
 
-	var b = browserify();
+	var b = browserify('./modules/qatan.js');
 	var out = require('fs').createWriteStream('./js/app.js',{flags: 'w+'});
 
-	b.add('./modules/qatan.js');
+	b.transform('brfs');
 	b.bundle().pipe(out);
 
 	console.log('Build complete');
